ccb76530895bb19da890f69213cb68739b6cc4d6
Jesus D'mitri Medina Novelo <72107871+DimitriMedNov@users.noreply.github.com>
2023-03-31 21:37:36 -0600

    Delete SpringMedinaNovelo directory

--- SpringMedinaNovelo/src/main/java/com/desarrollo/demospring/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.desarrollo.demospring.repositories;
-
-import com.desarrollo.demospring.entities.Product;
-import org.springframework.data.jpa.repository.*;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
